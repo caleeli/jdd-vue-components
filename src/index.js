0a372903b0874ae5fbb0d4df94333859786dbe31
David Callizaya <davidcallizaya@gmail.com>
2019-02-23 07:33:02 -0400

    Add boot function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import $ from 'jquery';
+import moment from 'moment';
+
 /**
  * COMPONENTES
  */
@@ -17,3 +20,17 @@ apis.keys().map(key => {
     const name = key.match(/\w+/)[0];
     api[name] = apis(key).default;
 });
+
+export const boot = function () {
+    // Boot
+    window._ = require('lodash');
+    window.Popper = require('popper.js').default;
+    window.jQuery = $;
+    window.$ = $;
+    global.jQuery = $;
+    require('clockpicker/dist/bootstrap-clockpicker.js');
+    require('bootstrap-datepicker/js/locales/bootstrap-datepicker.es.js');
+    moment.locale('es');
+    window.Vue = Vue;
+    Vue.config.productionTip = false;
+}
